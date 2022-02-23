634099bb00495a58ab172a0dcf6a4e743ddcb8ce
Darby Behnken <dtbehnken67@gmail.com>
2022-02-23 15:56:06 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(dis){
+return Math.abs(dis - 42);
+}
+
+function distanceFromHqInFeet(dis){
+    return Math.abs((dis - 42)*264)
+}
+
+function distanceTravelledInFeet(dis, drop){
+    return Math.abs((dis - drop)*264)
+}
+
+function calculatesFarePrice(start, stop){
+    const distance = Math.abs((start - stop)*264)
+
+    if (distance <= 400){
+        return 0
+    }
+    else if (distance <= 2000){
+        return Math.abs((distance - 400)*0.02)
+    }
+    else if (distance <= 2500){
+        return 25
+    }
+    else {
+        return 'cannot travel that far'
+    }
+}
+
